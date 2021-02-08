a5874cece2f049212081b7bc6d04b886117dce1e
John Glen <jglendening11@gmail.com>
2021-02-07 19:30:43 -0600

    adding greatest profit

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,7 +6,7 @@ total_profit_loss_amount = 0.00
 # Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
 average_profit_loss = 0.00
 # The greatest increase in profits(date and amount) over the entire period
-greatest_increase = {}
+greatest_increase = {"date": "", "amount": 0}
 # The greatest decrease in losses(date and amount) over the entire period
 greatest_decrease = {}
 
@@ -18,14 +18,28 @@ with open(file_path) as csvfile:
     print(f"CSV Header: {csv_header}")
 
     for row in csvreader:
+     # The total number of months included in the dataset
         total_months = total_months + 1
-        print(row)
-        total_profit_loss_amount = float(row[1])
-        print(row)
+        date = row[0]
+        profit = float(row[1])
+
+    # The net total amount of "Profit/Losses" over the entire period
+
+    # Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+
+    # The greatest increase in profits(date and amount) over the entire period
+        if (profit > greatest_increase["amount"]):
+            greatest_increase["date"] = date
+            greatest_increase["amount"] = profit
+    # The greatest decrease in losses(date and amount) over the entire period
+        if (profit < greatest_decrease["amount"]):
+            greatest_decrease["date"] = date
+            greatest_decrease["amount"] = profit
 print("financial Analysis")
 print("----------------------------------")
 print(f"total_months: {total_months}")
-print(f"total_profit_loss_amount: {total_profit_loss_amount}")
+print(
+    f"Greatest Increase in Profits: {greatest_increase['date']} (${greatest_increase['amount']})")
 
 # results should look like
 # Financial Analysis
